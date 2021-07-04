85ed35df7b5c4d48080d1e5b0653a7880e32febb
Ayush P Gupta <ayushpguptaapg@gmail.com>
2021-07-04 10:27:43 +0530

    Fix complete error not displayed
    Signed-off-by: Ayush P Gupta <ayushpguptaapg@gmail.com>

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -2,9 +2,7 @@
   <v-container class="py-8">
     <v-row class="text-center" justify="center">
       <v-col class="mb-2" cols="6">
-        <h1 class="headline-6 font-weight-bold ">
-          Sprint Notes Generator
-        </h1>
+        <h1 class="headline-6 font-weight-bold">Sprint Notes Generator</h1>
         <div v-if="!loading">
           <div class="text-overline text-start mt-4">Azure PAT</div>
           <v-text-field
@@ -16,60 +14,64 @@
             type="password"
           ></v-text-field>
           <div class="text-overline text-start mt-4">Iteration Path(s)</div>
-          <v-text-field v-for="(iteration) in iterationPaths" :key="iteration.id"
-                        v-model="iteration.path"
-                        class="mb-4"
-                        hide-details
-                        outlined
-                        placeholder="Sample Path"
+          <v-text-field
+            v-for="iteration in iterationPaths"
+            :key="iteration.id"
+            v-model="iteration.path"
+            class="mb-4"
+            hide-details
+            outlined
+            placeholder="Sample Path"
           ></v-text-field>
           <v-btn color="primary" v-on:click="addIterationPath">+ ADD</v-btn>
         </div>
         <div class="mt-8">
           <div v-if="error" class="red--text my-4">{{ error }}</div>
-          <v-btn v-if="!loading" color="green" dark v-on:click="generate">GENERATE SPRINT NOTES
+          <v-btn v-if="!loading" color="green" dark v-on:click="generate"
+            >GENERATE SPRINT NOTES
           </v-btn>
-          <v-progress-circular v-else color="primary" indeterminate width="2"/>
+          <v-progress-circular v-else color="primary" indeterminate width="2" />
         </div>
-
       </v-col>
       <v-col v-if="content" class="mb-2" cols="12">
-        <h1 class="headline-6 font-weight-bold ">
-          Preview
-        </h1>
-        <v-md-editor v-model="content" class="my-4 text-start" default-fullscreen="true" height="600px"
-                     @copy-code-success="handleCopyCodeSuccess"></v-md-editor>
-
+        <h1 class="headline-6 font-weight-bold">Preview</h1>
+        <v-md-editor
+          v-model="content"
+          class="my-4 text-start"
+          default-fullscreen="true"
+          height="600px"
+          @copy-code-success="handleCopyCodeSuccess"
+        ></v-md-editor>
       </v-col>
     </v-row>
-
-
   </v-container>
 </template>
 
 <script lang="ts">
-import Vue from 'vue'
+import Vue from "vue";
 
-const axios = require('axios');
+const axios = require("axios");
 
 export default Vue.extend({
-  name: 'HelloWorld',
+  name: "HelloWorld",
   components: {},
   data: () => ({
     loading: false,
     showParsed: false,
-    error: '',
-    azToken: '',
-    content: '',
-    org: 'flick2know',
-    iterationPaths: [{
-      id: 1,
-      path: "Field_Assist\\FA Team 2\\Sprint 118 T2"
-    }],
+    error: "",
+    azToken: "",
+    content: "",
+    org: "flick2know",
+    iterationPaths: [
+      {
+        id: 1,
+        path: "Field_Assist\\FA Team 2\\Sprint 118 T2",
+      },
+    ],
   }),
   methods: {
     addIterationPath() {
-      this.iterationPaths.push({ id: Date.now(), path: "Sample/Path" })
+      this.iterationPaths.push({ id: Date.now(), path: "Sample/Path" });
     },
     async generate() {
       try {
@@ -78,26 +80,27 @@ export default Vue.extend({
           throw "token,org,iteration cannot be null/empty!";
         }
         this.loading = true;
-        const paths = this.iterationPaths.map(value => value.path);
+        const paths = this.iterationPaths.map((value) => value.path);
         const config = {
-          method: 'get',
-          url: `https://fa-azure-api.azurewebsites.net/generate?token=${ this.azToken }&org=${ this.org }&iterationPaths=${ JSON.stringify(paths) }`,
-          headers: {}
+          method: "get",
+          url: `https://fa-azure-api.azurewebsites.net/generate?token=${
+            this.azToken
+          }&org=${this.org}&iterationPaths=${JSON.stringify(paths)}`,
+          headers: {},
         };
 
         const response = await axios(config);
         this.content = response.data;
       } catch (e) {
         console.error(e);
-        this.error = e?.toString();
+        this.error = `${e.toString()}\n${e.response.data}`;
       } finally {
         this.loading = false;
       }
     },
     handleCopyCodeSuccess(code: any) {
       console.log(code);
-
-    }
-  }
-})
+    },
+  },
+});
 </script>
